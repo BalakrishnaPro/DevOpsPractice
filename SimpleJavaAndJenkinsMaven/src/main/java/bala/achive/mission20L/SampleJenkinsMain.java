1f6485609dda32041246f4cd2719bc90b5ca8e35
BalakrishnaPro <bala.csit@gmail.com>
2019-07-07 03:03:07 +0530

    Added Functional Interface and used it in the main class.

--- SimpleJavaAndJenkinsMaven/src/main/java/bala/achive/mission20L/SampleJenkinsMain.java
+++ SimpleJavaAndJenkinsMaven/src/main/java/bala/achive/mission20L/SampleJenkinsMain.java
@@ -1,10 +1,17 @@
 package bala.achive.mission20L;
 
+import bala.achive.mission20L.functional.SampleFunctionalInterface;
+
 public class SampleJenkinsMain {
 
 	public static void main(String[] args) {
 
 		System.out.println("Hello Jenkins Application...");
+		printName(name -> System.out.println(name));
+	}
+	
+	public static void printName(SampleFunctionalInterface sfi) {
+		sfi.sampleFucntion("Hello Balakrishna!!");
 	}
 
 }
---
+++ SimpleJavaAndJenkinsMaven/src/main/java/bala/achive/mission20L/functional/SampleFunctionalInterface.java
@@ -0,0 +1,7 @@
+package bala.achive.mission20L.functional;
+
+@FunctionalInterface
+public interface SampleFunctionalInterface {
+
+	void sampleFucntion(String name);
+}
